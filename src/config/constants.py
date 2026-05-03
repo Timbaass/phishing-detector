ef6b04ab017883cf79ca6d7de4d7069cdf6c09d3
Timbbass <msozgur44@gmail.com>
2026-05-03 14:27:23 +0300

    new: feature column names added for feature extractor.

--- src/config/constants.py
+++ src/config/constants.py
@@ -15,8 +15,18 @@ URGENT_KEYWORDS = {
     "update",
 }
 
+NUMERIC_FEATURES = [
+    "url_count",
+    "contains_urgent_word",
+    "body_len",
+    "is_html",
+    "exclamation_count",
+    "time_bucket",
+]
+
+TEXT_COLUMNS = ["subject", "body"]
+
 STOP_WORDS = set(ENGLISH_STOP_WORDS)
 
 URL_PATTERN = re.compile(r"(https?://\\S+|www\\.\\S+)", flags=re.IGNORECASE)
 HTML_PATTERN = re.compile(r"<[^>]+>")
-PUNCT_TABLE = str.maketrans("", "", string.punctuation)
